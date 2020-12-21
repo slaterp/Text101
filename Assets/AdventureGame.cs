99c7f65fc248c80403db33d6cd4ac076a24c46b4
Patrick <patrickslater97@gmail.com>
2020-12-21 19:40:36 +1100

    Initial commit. Game file

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+
+    [SerializeField] Text textComponent; // SerializeField makes textcomponent available in Unity engine
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = "hello world";
+
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
