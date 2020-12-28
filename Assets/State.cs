894368e4dd9e9b89bea529a914fff1ceb8c63dc4
Patrick <patrickslater97@gmail.com>
2020-12-28 13:39:42 +1100

    Added initial state and corrresponding files for state creation

---
+++ Assets/State.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+
+    [TextArea(10, 14)] [SerializeField] string storyText; // TextArea() generates a text area to write in and defines it size in brackets
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+
+}
